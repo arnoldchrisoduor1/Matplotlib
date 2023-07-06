647ed3e37f1a231de9d9b7d71822c20e46df8749
Arnold Chris <arnoldchrisoduor@gmail.com>
2023-07-06 12:47:58 +0300

    scatter

--- scatter.py
+++ scatter.py
@@ -25,18 +25,18 @@ x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
 y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
 
 colors = np.array([0, 10, 20, 30, 40, 45, 50, 55, 60, 70, 80, 90, 100])
-# plt.scatter(x, y,c= colors, cmap = 'viridis')
+plt.scatter(x, y,c= colors, cmap = 'viridis')
 
-# plt.colorbar()
-# plt.show()
+plt.colorbar()
+plt.show()
 
 #setting sizes for the markers.
-# x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
-# y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
-# sizes = np.array([20,50,100,200,500,1000,60,90,10,300,600,800,75])
+x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+sizes = np.array([20,50,100,200,500,1000,60,90,10,300,600,800,75])
 
-# plt.scatter(x, y, s = sizes, alpha=0.5)
-# plt.show()
+plt.scatter(x, y, s = sizes, alpha=0.5)
+plt.show()
 
 #Pretty
 x = np.random.randint(100, size=100)
