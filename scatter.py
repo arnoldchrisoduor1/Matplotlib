edc0ce3387b4976f7e275d9ee397a4232055f0a5
Arnold Chris <arnoldchrisoduor@gmail.com>
2023-07-06 12:19:52 +0300

    scatter

---
+++ scatter.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#comparing the age and the spee of the cars.
+# x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+# y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+# plt.scatter(x, y, color = 'blue')
+
+# x = np.array([2,2,8,1,15,8,12,9,7,3,11,4,7,14,12])
+# y = np.array([100,105,84,105,90,99,90,95,94,100,79,112,91,80,85])
+# plt.scatter(x, y, color = 'orange')
+
+# plt.show()
+
+#Specifying the colors for each dot.
+# x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+# y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+# colors = np.array(["red","green","blue","yellow","pink","black","orange","purple","beige","brown","gray","cyan","magenta"])
+# plt.scatter(x, y, c = colors)
+
+# plt.show()
+
+#Using a color map 'virdis'
+x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+
+colors = np.array([0, 10, 20, 30, 40, 45, 50, 55, 60, 70, 80, 90, 100])
+# plt.scatter(x, y,c= colors, cmap = 'viridis')
+
+# plt.colorbar()
+# plt.show()
+
+#setting sizes for the markers.
+# x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+# y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+# sizes = np.array([20,50,100,200,500,1000,60,90,10,300,600,800,75])
+
+# plt.scatter(x, y, s = sizes, alpha=0.5)
+# plt.show()
+
+#Pretty
+x = np.random.randint(100, size=100)
+y = np.random.randint(100, size=100)
+colors = np.random.randint(100, size=100)
+sizes = 10*np.random.randint(100, size=100)
+
+plt.scatter(x, y, c=colors,alpha=0.5, s=sizes, cmap='nipy_spectral')
+plt.colorbar()
+plt.show()
