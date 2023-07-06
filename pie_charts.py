72ae427e462446ca089113e5714fa053b53f9be0
Arnold Chris <arnoldchrisoduor@gmail.com>
2023-07-06 12:46:31 +0300

    pie charts

--- pie_charts.py
+++ pie_charts.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# y = np.array([35, 25, 25, 15,])
-# mylabels = ["Cherry", "Apples", "Bananas", "Mangoes"]
-# myexlode = [0.2, 0, 0, 0]
-# plt.pie(y, labels = mylabels, startangle=90, explode=myexlode, shadow=True)
-# plt.show()
+y = np.array([35, 25, 25, 15,])
+mylabels = ["Cherry", "Apples", "Bananas", "Mangoes"]
+myexlode = [0.2, 0, 0, 0]
+plt.pie(y, labels = mylabels, startangle=90, explode=myexlode, shadow=True)
+plt.show()
 
 #Specifying the colors.
 y = np.array([35, 25, 25, 15,])
