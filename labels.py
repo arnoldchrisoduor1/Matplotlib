ae55b1911024333392655aaa007b4a13509c422e
Arnold Chris <arnoldchrisoduor@gmail.com>
2023-07-06 13:07:11 +0300

    labels

--- labels.py
+++ labels.py
@@ -5,11 +5,11 @@ import numpy as np
 x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
 y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
 
-# plt.plot(x, y)
-# plt.title("Sports watch Data")
-# plt.xlabel("Average Pulse")
-# plt.ylabel("Calorie burnage")
-# plt.show()
+plt.plot(x, y)
+plt.title("Sports watch Data")
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie burnage")
+plt.show()
 
 #Setting fonts.
 x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
